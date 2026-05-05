bdc72919fc52a1a09194e9834eca2ae4dccbac13
networkcom-design <networckcom@gmail.com>
2026-05-05 01:04:18 -0300

    Update QrService.java

--- backend/src/main/java/com/gymsystem/service/QrService.java
+++ backend/src/main/java/com/gymsystem/service/QrService.java
@@ -1,11 +1,11 @@
 package com.gymsystem.service;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-
-
+import org.springframework.stereotype.Service;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
